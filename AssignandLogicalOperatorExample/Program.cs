4db0e04c271e51ef4d64d09d47c6045d03e63c6c
Gautham V <gowthamsiva316@gmail.com>
2023-02-22 23:18:48 +0530

    changes made for string example and assignexample

--- AssignandLogicalOperatorExample/Program.cs
+++ AssignandLogicalOperatorExample/Program.cs
@@ -22,6 +22,30 @@ internal class Program
         Console.WriteLine("The addequal is :" + addequal);
         Console.WriteLine("The multiplyequal is :" + multiplyequal);
 
+        int x = 5;
+        x++;   //+1
+        x--;   //-1             
+        x += 3;
+        x *= 2;
+        x -= 5;
+
+
+        int Z = 5;
+        int y = 3;
+        int Result = 5;
+        Console.WriteLine(Result > 3 && Result < 10);
+        Console.WriteLine(x > 3 || x < 4);
+        int C = 5;
+        Console.WriteLine(!(C > 3 && C < 10));             //Console.WriteLine(!(x > 3 && x < 10));
+        Console.WriteLine(Z != y);
+        Console.WriteLine(Z > y); // returns True because 5 is greater than 3   
+
+        Console.WriteLine(Math.Min(5, 10));  //For Min Value            
+        Console.WriteLine(Math.Max(5, 10));  //For Max Value 
+        Console.WriteLine(Math.Sqrt(81));    //For  square root              
+        Console.WriteLine(Math.Abs(-4.7));   //For Positive value of Number             
+        Console.WriteLine(Math.Round(9.52));
+        Console.WriteLine(x);
 
 
     }
---
+++ StringExample/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+internal class Program
+{
+    /// <summary>
+    /// 
+    /// 
+    /// </summary>
+    /// <param name="args">Main </param>
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+
+        string Fname = "GAUTHAM";
+        string Lastname = "VAJRAM";
+        int age = 27;
+        string Fullname = Fname+ Lastname;
+
+        Console.WriteLine("My Fullname is" + Fullname + " " + "and my age is" + " " + age);
+
+        Console.WriteLine("My Fullname is" + string.Concat(Fname+Lastname) + " " + "and my age is" + " " + age);
+
+        Console.WriteLine("My Fullname is" + string.Concat(Fname + Lastname) + "@" + age);
+
+        Console.WriteLine($"My Fullname is  {Fname} {Lastname} and age is {age} ");    //$ ->string Interpolation 
+
+        Console.WriteLine("My Fullname is {0} {1}  and age is {2} ", "Fname","Lastname","age"); //--> string Interpolation2
+
+        string Name = "   GAUTHAM  ";
+        
+        string whitespace = Name.Replace(" ", string.Empty);
+
+        Console.WriteLine(Name.Trim());
+
+
+        Console.Write("Enter your Age:");
+        String strage = Console.ReadLine();
+        int Age = Convert.ToInt32(strage);
+        Console.WriteLine("You are " + Age + " year old");
+
+
+
+        Console.Write("Enter your Age1:");
+        String strage1 = Console.ReadLine();
+        int Age1 = int.Parse(strage1);
+        Console.WriteLine("You are " + Age1 + " year old");
+
+
+      
+
+
+
+
+    }
+     
+
+}
