56afeb798f46ea9ba4f1fc26f553d8721f2c9e88
Gautham V <gowthamsiva316@gmail.com>
2023-02-24 20:20:32 +0530

    Added in ifelse and switch program

---
+++ ifelseProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("age:");
+        double age1 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+        if (age1 == 18)
+        {
+            Console.WriteLine("Eligible for marriage");
+        }
+        else if (age1 > 18)
+        {
+            Console.WriteLine("You can get married");
+
+        }
+        else
+        {
+            Console.WriteLine("You be single forever");
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ switchProgram/Program.cs
@@ -0,0 +1,60 @@
+﻿//Console.WriteLine("Hello, World!");
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //int day = 4;
+        //switch (day)
+        //{
+        //    case 1:
+        //        Console.WriteLine("Monday");
+        //        break;
+        //    case 2:
+        //        Console.WriteLine("Tuesday");
+        //        break;
+        //    case 3:
+        //        Console.WriteLine("Wednesday");
+        //        break;
+        //    case 4:
+        //        Console.WriteLine("Thursday");
+        //        break;
+        //    case 5:
+        //        Console.WriteLine("Friday");
+        //        break;
+        //    case 6:
+        //        Console.WriteLine("Saturday");
+        //        break;
+        //    case 7:
+        //        Console.WriteLine("Sunday");
+        //        break;
+        //}
+
+        {
+
+            Console.WriteLine("please input A");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please input B");
+
+            int b = Convert.ToInt32(Console.ReadLine()); string op = Console.ReadLine(); switch (op)
+
+            {
+
+                //It's a placeholder in the string.
+
+                case "+": Console.WriteLine("addition of two numbers is{0}", a + b); break;
+
+                case "-": Console.WriteLine("substraction of two numbers is{0}", a - b); break;
+
+                case "*": Console.WriteLine("multiplication of two numbers is{0}", a * b); break;
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
+ 
