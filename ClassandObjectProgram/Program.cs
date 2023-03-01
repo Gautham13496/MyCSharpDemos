0fad4b6432bf8af1850ac9a7fc7bae37d9ec65de
Gautham V <gowthamsiva316@gmail.com>
2023-03-01 20:34:09 +0530

    Added code in SingleClassObject and ClassandObjectProgram

---
+++ ClassandObjectProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿
+    class Empdata
+    { 
+   
+        public string Empname = "Siva";
+        public int EmpId = 3831;
+        class Emp
+        {
+            static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            Empdata empdetails = new Empdata();
+            Empdata empfulldetails = new Empdata();
+
+            Console.WriteLine(empdetails.Empname + " " + empdetails.EmpId);
+            Console.WriteLine(empfulldetails.Empname + " " + empfulldetails.EmpId);
+        }
+    }
+}
---
+++ SingleClassObject/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    class Emp
+    {
+        string Empname = "Gautham";
+        int EmpId = 3831;
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            Emp empdetails = new Emp();
+            Emp empfulldetails = new Emp();
+            
+            Console.WriteLine(empdetails.Empname +" " +empdetails.EmpId);
+            Console.WriteLine(empfulldetails.Empname +" " +empfulldetails.EmpId);
+        }
+    }
+}
