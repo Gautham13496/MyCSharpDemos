c2a5e0e961c6d02dc1631cce67cf2e664a27858d
Gautham V <gowthamsiva316@gmail.com>
2023-03-06 20:31:08 +0530

    added code in InternalModifier

---
+++ InternalModifier/Program.cs
@@ -0,0 +1,23 @@
+﻿class emp
+{
+    internal string empname = "Gautham";
+    internal int empid = 3831;
+
+    public void Namedetails()
+
+    {
+        Console.WriteLine("Name is:" + empname + "," + "id is:" + empid);
+
+
+    }
+}
+    public class Program
+{
+     static void Main(string[] args)
+    {
+        emp theemp= new emp();
+        Console.WriteLine("Name:" + theemp.empname);
+       theemp.Namedetails();
+        Console.WriteLine("\nPress any key to exit from the window....");
+    }
+}
