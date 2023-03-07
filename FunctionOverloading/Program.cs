69a6b04ac23a05c30505970d26d165800400ffdb
Gautham V <gowthamsiva316@gmail.com>
2023-03-07 20:31:05 +0530

    added code in FunctionProgram and FunctionOverloading

---
+++ FunctionOverloading/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static int Addition(int x, int y)
+    { 
+        return x + y; 
+    }
+    static double Addition(double x, double y)
+    {
+        return x + y;
+    }
+
+    static void Main(string[] args)
+    {
+        
+        int i = Program.Addition(1, 2);
+        Console.WriteLine("Int:" +i);
+        double j = Program.Addition(1.3, 1.2);
+        Console.WriteLine("Double:" +j);
+
+    }
+}
---
+++ FunctionProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static int plusmethodInt(int x,int y)
+    { return x + y; }
+
+    static void Main(string[] args)
+    {
+        Program pr = new Program();
+       int i =  Program.plusmethodInt(1, 2);
+        
+        Console.WriteLine(i);
+
+        
+    }
+}
