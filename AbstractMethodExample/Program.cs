401fbebad338057d48c8d1a5a940e0049b2a4ed1
Gautham V <gowthamsiva316@gmail.com>
2023-03-14 22:37:22 +0530

    Added code in SealedKeywordExample and AbstractMethodExample

---
+++ AbstractMethodExample/Program.cs
@@ -0,0 +1,60 @@
+﻿internal class Program
+{
+    public abstract class Shape
+
+    {
+
+        public abstract void draw();
+
+    }
+
+    public class Rectangle : Shape
+
+    {
+
+        public override void draw()
+
+        {
+
+            Console.WriteLine("drawing rectangle...");
+
+        }
+
+    }
+
+    public class Circle : Shape
+
+    {
+
+        public override void draw()
+
+        {
+
+            Console.WriteLine("drawing circle...");
+
+        }
+
+    }
+
+    public class TestAbstract
+
+    {
+
+        public static void Main()
+
+        {
+
+            Shape s;
+
+            s = new Rectangle();
+
+            s.draw();
+
+            s = new Circle();
+
+            s.draw();
+
+        }
+
+    }
+}
---
+++ SealedKeywordExample/Program.cs
@@ -0,0 +1,40 @@
+﻿sealed public class Animal
+
+{
+    public void eat()
+
+    { Console.WriteLine("eating...");    }
+}
+
+public class Dog : Animal
+
+{
+
+    public void bark()
+
+    { Console.WriteLine("barking..."); }
+
+}
+
+public class TestSealed
+
+{
+
+    public static void Main()
+
+    {
+
+        Dog d = new Dog();
+
+        Animal an = new Animal();
+
+        d.eat();
+
+        d.bark();
+
+        an.eat();
+
+    }
+
+}
+
