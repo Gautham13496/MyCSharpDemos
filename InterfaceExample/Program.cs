9ff86f7969302130dd3e76cd384e6f6a4f93d7e5
Gautham V <gowthamsiva316@gmail.com>
2023-03-15 20:33:41 +0530

    Added code in InterfaceExample

---
+++ InterfaceExample/Program.cs
@@ -0,0 +1,54 @@
+﻿interface IMenu
+
+{
+
+    public void UN(); // interface method
+
+}
+interface ILoginPage
+
+{
+
+    void PW(); // interface method
+
+}     // Implement multiple interfaces
+
+class TrailClass :IMenu, ILoginPage
+
+{
+
+    public void UN()
+
+    {
+
+        Console.WriteLine("Username is:Gautham");
+
+    }
+
+    public void PW()
+
+    {
+
+        Console.WriteLine("Password is:DOTNET");
+
+    }
+
+}
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        TrailClass myObj = new TrailClass();
+
+        myObj.UN();
+
+        myObj.PW();
+        Console.ReadLine();
+
+    }
+
+}
