fac9331024b450511f99c5329f22a2f3fcff71db
Gautham V <gowthamsiva316@gmail.com>
2023-03-16 19:58:16 +0530

    Added code in TryCatchExceptionHandlingExample

---
+++ TryCatchExceptionHandlingExample/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int [] pincode = {5,1,7,0,0};
+            Console.WriteLine(pincode[6]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Pincode is not valid....");
+        }
+        finally { Console.WriteLine("Please provide a valid 6 digit pincode!"); }
+        Console.ReadLine();
+    
+    }
+}
